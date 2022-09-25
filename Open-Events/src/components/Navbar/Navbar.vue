2a8a60f197948d1fb07daf66630988e1b9104484
pcoderch <93677392+pcoderch@users.noreply.github.com>
2022-09-25 23:52:33 +0200

    started navbar, first need to create branches

---
+++ Open-Events/src/components/Navbar/Navbar.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+        props:{},
+        setup() { 
+        }
+    }
+</script>
+
+<template>
+    <div class="navbar">
+        <div class="navbar__logo"></div>
+    </div>
+</template>
