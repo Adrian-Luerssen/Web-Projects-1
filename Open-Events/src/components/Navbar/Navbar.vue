e0de8885b66e4c1b42fef5e112f2a465848d6c98
pcoderch <93677392+pcoderch@users.noreply.github.com>
2022-09-26 03:15:27 +0200

    finished navbar frontend, need to implement connections when clicked

--- Open-Events/src/components/Navbar/Navbar.vue
+++ Open-Events/src/components/Navbar/Navbar.vue
@@ -1,3 +1,23 @@
+<template>
+  <CurvedBottomNavigation :options="options" v-model="selected" foreground-color='#B411D4'
+  badge-color='#FBC02D'
+  background-color='#FFFFFF'
+  icon-color='#0000008A'>
+      <template #icon="{ props }">
+        <link rel="stylesheet" 
+        href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" 
+        integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" 
+        crossorigin="anonymous">
+          <i :class="props.icon"></i>
+      </template>
+      <template #title="{ props }">
+        <b>{{ props.title }}</b>
+      </template>
+  </CurvedBottomNavigation>
+
+  </template>
+
+
 <script>
     import { CurvedBottomNavigation } from "bottom-navigation-vue";
     import "bottom-navigation-vue/dist/style.css";
@@ -13,36 +33,27 @@
         options: [
           {
             id: 1,
-            icon: fasTwitter,
-            title: "Home",
+            icon: "fas fa-home",
+            title: "Home"
           },
           { id: 2,
             icon: "fas fa-wallet",
-            title: "Wallet" },
+            title: "Wallet"
+          },
           {
             id: 3,
             icon: "fas fa-plus",
-            title: "Setting",
+            title: "Setting"
           },
           { id: 4,
             icon: "fas fa-bell",
-            title: "Notification",
-        },
+            title: "Notification"
+          },
         ],
       }),
     };
   </script>
+  
 
-<template>
 
-<CurvedBottomNavigation :options="options" v-model="selected">
-    <template #icon="{ props }">
-        <font-awesome-icon> {{props.icon}}</font-awesome-icon>
-    </template>
-    <template #title="{ props }">
-      <b>{{ props.title }}</b>
-    </template>
-  </CurvedBottomNavigation>
-
-  </template>
   
--- Open-Events/src/main.js
+++ Open-Events/src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import bottomNavigationVue from "bottom-navigation-vue";
 import "bottom-navigation-vue/dist/style.css";
