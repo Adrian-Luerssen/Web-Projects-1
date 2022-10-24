e1a93fbe5d6912299be99e8fde22fe73f0de5d9b
maria.duch <maria.duch@students.salle.url.edu>
2022-10-24 16:08:06 +0200

    final

---
+++ Open-Events/src/components/Events/Events.vue
@@ -0,0 +1,118 @@
+
+<template>    
+    <div class="box">
+      <div class="side_panel">
+        <p>This is a side panel</p>
+      </div>
+      <v-list>
+      <v-list-item v-for="item in elements" :key="item.event" two-line>
+        <div class="event_box">
+          <img id="img" src="https://fotografias.antena3.com/clipping/cmsimages02/2022/09/13/4E016859-5F7C-46D3-92AB-E79CA8345834/mercedes-benz-fashion-week-madrid-2022-semana-moda-madrid_98.jpg?crop=1920,1080,x0,y0&width=1900&height=1069&optimize=low&format=webply">
+          <h3 class="title">{{item.event}}</h3>
+          <p class="description">{{item.description}}</p>
+        </div>
+        <div class="empty">
+          <!--Ask to change this-->
+        </div>
+    </v-list-item>
+    </v-list>
+    </div>
+    
+  </template>
+  
+  <script>
+  export default {
+    data: () => ({
+      elements: [
+        { event: "Oktoberfest", 
+          description: "The Oktoberfest is the world's largest Volksfest, featuring a beer festival and a travelling funfair.",
+          img: "https://www.thebeertimes.com/wp-content/uploads/2017/09/Oktoberfest-Munich.jpg"},
+        { event: "NYFW", 
+          description: "Officially called Mercedes-Benz Fashion Week, annual fashion week event held in New York City considered the most important event in the fashion industry.",
+          img: "https://fotografias.antena3.com/clipping/cmsimages02/2022/09/13/4E016859-5F7C-46D3-92AB-E79CA8345834/mercedes-benz-fashion-week-madrid-2022-semana-moda-madrid_98.jpg?crop=1920,1080,x0,y0&width=1900&height=1069&optimize=low&format=webply" },
+        { event: "Coachella", 
+          description: "The Coachella Valley Music and Arts Festival is a major music festival that takes place over three days in the last week of April in Indio, California.",
+          img: "https://images.ecestaticos.com/wx-uXc24uN-tnn9ei0Bljt-3qW4=/0x118:2271x1394/1600x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F1c7%2Fa35%2F419%2F1c7a354190cbb2c578c5c4f3e0c82a52.jpg"},
+        { event: "Sallefest", 
+          description: "The party you have been waiting all year is back. During a week full of activities, the boredom is the only one not welcome.",
+          img: "https://i.ytimg.com/vi/FGs_66qxW2U/maxresdefault.jpg"},
+        { event: "Movies & Wine", 
+          description: "A perfect date to spend some time with the ones you love or just to be by yourself, this is a great opportunity to relax and ejoy calm.",
+          img: "https://www.winemag.com/wp-content/uploads/2015/03/WE_Movies0336.jpg" }
+      ]
+    })
+  };
+  
+  function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+  </script>
+  <style>
+  body {
+        min-height: 100vh;
+        background: #E3A2BA;
+        box-sizing: border-box;
+        font-weight: normal;
+        position: relative;
+    }
+    
+    *{
+        margin: 0;
+        width: auto;
+        height: auto;
+        
+    }
+    .side_bar{
+      
+      width: 200px;
+      background-color: #401826;
+    }
+    .event_box{
+        margin-left: 15%;
+        font-family: Arial, Helvetica, sans-serif;
+        border-radius: 25px;
+        background-color: #BF6183;
+        height: 150px;
+        width: 600px;
+        border: solid#E3A2BA;
+        gap: 10px;
+        display: grid;
+        grid-template-columns: 200px 400px;
+        grid-template-rows: 50px 100px;
+        align-items: left;
+        padding: 10px 10px;
+    }
+    .empty{
+        background-color: #E3A2BA;
+        padding: 10px 100px;
+    }
+    #img{
+      grid-column: 1;
+      width: 190px;
+      border-top-left-radius: 50% 50%; 
+      border-top-right-radius: 50% 50%; 
+      border-bottom-right-radius: 50% 50%; 
+      border-bottom-left-radius: 50% 50%;
+      margin: 7%;
+    }
+    .title{
+      grid-column: 2;
+      grid-row: 1;
+      color: white;
+      font-weight: bold;    
+    }
+    .description{
+      grid-column: 2;
+      grid-row: 2;
+      color: rgb(255, 213, 242);
+      
+    }
+    .side_panel{
+      background-color: #401826;
+    }
+  </style>
---
+++ Open-Events/src/components/Profile/Profile.vue
@@ -0,0 +1,151 @@
+<template>
+    <div class="box">
+          <div class="profile">
+              <h1>Profile</h1>
+              <img id="img" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png">
+              <input type="file" id="profile_photo" accept="image/png, image/jpeg" >
+          </div>
+          <div class="information">
+            <div class="div_name">
+              <h4 id="First_name" class="text">
+                First name: </h4>
+              <input type="text" id= "first_name_text" name="first_name"
+                placeholder="User" readonly>
+              <button type="button" id="change_first_name_button" class="button_change">
+                Change...</button>
+            </div>
+            <div class="div_last">
+              <h4 id="Last_name" class="text">
+                Last name: </h4>
+              <input type="text" id= "last_name_text" name="last_name"
+                placeholder="Surname" readonly>
+              <button type="button" id="change_last_name_button" class="button_change" >
+                Change...</button>
+            </div>
+            <div class="div_email">
+              <h4 id="Email" class="email">
+                Email: </h4>
+              <input type="text" id= "email_text" name="email"
+                placeholder="user@gmail.com" readonly>
+              <button type="button" id="change_email_button" class="button_change" >
+                Change...</button>
+            </div>
+            <div class="div_password">
+              <h4 id="Password" class="password">
+                Password: </h4>
+              <input type="text" id= "password_text" name="password"
+                placeholder="ABCD1234" readonly>
+              <button type="button" id="change_password_button" class="button_change" >
+                Change...</button>
+            </div>      
+          </div>
+    </div>
+  </template>
+  
+  <script>
+  /*
+  var change_name = true; 
+  const button_name = document.getElementById("change_first_name_button");
+  button_name.addEventListener("click", changeNameFunction);
+  
+  function changeNameFunction() {
+      if(change_name == true){
+          document.getElementById("first_name_text").readOnly = false;
+          change_name=false;
+      }else{
+          document.getElementById("first_name_text").readOnly = true;
+          change_name=true;
+      }
+  } 
+  */
+  </script>
+  <style>
+  body {
+      min-height: 100vh;
+      background: linear-gradient(
+        to right,
+        #E3A2BA 0%,
+        #E3A2BA 50%,
+        #BF6183 50%,
+        #BF6183 100%
+      );
+      box-sizing: border-box;
+      font-weight: normal;
+      position: relative;
+  }
+  
+  *{
+      margin: 0;
+      width: auto;
+      height: auto;
+      
+  }
+  
+  .box{
+      font-family: Arial, Helvetica, sans-serif;
+      color: white;
+      display:flex;
+      flex-direction: row;
+      height: 600px;
+      width: 900px;
+  }
+  
+  .profile{
+      align-items: center;
+      color: white; 
+      margin-left: -10mm; 
+      margin-top: 10%;
+  }
+  #img{
+      width: 300px;
+      padding: 40px;
+  }
+  
+  .information{
+      margin-top: 15%;
+      margin-left: 15%;
+      width: 40%;
+      height: 200px;
+      display: grid;
+      grid-row-gap: 20%;
+      grid-template-rows: repeat(4, 1fr);    
+  }
+  .div_name{
+      color: white;
+      white-space: nowrap;
+      display: grid;
+      grid-column-gap: 10%;
+      grid-template-columns: 70px 200px 100px;
+  }
+  .div_last{
+      color: white;
+      white-space: nowrap;
+      display: grid;
+      grid-column-gap: 10%;
+      grid-template-columns: 70px 200px 100px;
+  }
+  .div_email{
+      color: white;
+      white-space: nowrap;
+      display: grid;
+      grid-column-gap: 10%;
+      grid-template-columns: 70px 200px 100px;
+  }
+  
+  .div_password{
+      color: white;
+      white-space: nowrap;
+      display: grid;
+      grid-column-gap: 10%;
+      grid-template-columns: 70px 200px 100px;
+  }
+  
+  .button_change{
+      white-space: nowrap;
+      padding: 4px 10px;
+      color: black;
+      font-size: 12px;
+      background-color: #EBD68B;
+      border-radius: 20px;
+  }
+  </style>
